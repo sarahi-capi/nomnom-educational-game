e50698fa4d7d9c3240d1aa2f34fef4cb6fb8231d
Sarahi Capi Casillas <s240349@th-ab.de>
2025-07-01 20:54:08 +0200

    Adding the test results to the documentation and adding a margin system for the ingredients

--- Scripts/cheese.gd
+++ Scripts/cheese.gd
@@ -7,10 +7,23 @@ var mouse_offset = Vector2(0, 0)
 func _process(delta):
 	if selected:
 		followMouse()
-		
+
 func followMouse():
-	position = get_global_mouse_position() + mouse_offset
+	var mouse_position = get_global_mouse_position() + mouse_offset
 	
+	# This is the current screen size
+	var screen_size = get_viewport_rect().size
+	
+	# Margin so the item doesn't go off-screen
+	var margin = 60.0
+	
+	# Limit position based on dynamic screen size and margins
+	mouse_position.x = clamp(mouse_position.x, margin, screen_size.x - margin)
+	mouse_position.y = clamp(mouse_position.y, margin, screen_size.y - margin)
+	
+	global_position = mouse_position
+
+
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
@@ -18,7 +31,7 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 			selected = true
 		else:
 			selected = false
-			
+
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
 		selected = false
